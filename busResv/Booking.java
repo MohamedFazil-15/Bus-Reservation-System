a265c51aa380b796aac1c563a84a24d4a01a8556
Mohamed Fazil Y <119285492+MohamedFazil-15@users.noreply.github.com>
2024-07-26 22:52:46 +0530

    Add files via upload

---
+++ busResv/Booking.java
@@ -0,0 +1,50 @@
+package busResv;
+
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Booking {
+	
+	String passengerName;
+	int busNo;
+	Date date;
+	
+	Booking(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Name of Passenger : ");
+		passengerName = sc.next();
+		
+		System.out.println("Enter the Bus Number : ");
+		busNo = sc.nextInt();
+		
+		System.out.println("Enter Date (dd-mm-yyyy) : ");
+		String dateInput = sc.next();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		
+		try {
+			dateFormat.parse(dateInput);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean isAvailable(ArrayList<Booking> bookings, ArrayList<Bus> buses) {
+		int capacity = 0;
+		for(Bus bus:buses) {
+			if(bus.getBusNo() == busNo)
+				capacity = bus.getCapacity();
+		}
+		int booked = 0;
+		for(Booking b:bookings) {
+			if(b.busNo == busNo &&  b.date != null && b.date.equals(date)) {
+				booked++;
+			}else {
+				return false;
+			}
+		}
+		
+		return booked < capacity ? true : false;
+	}
+
+}
---
+++ busResv/BusDemo.java
@@ -0,0 +1,43 @@
+package busResv;
+import java.util.*;
+
+public class BusDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Bus> buses = new ArrayList<Bus>();
+		ArrayList<Booking> bookings = new ArrayList<Booking>();
+		buses.add(new Bus(1,true,1));
+		buses.add(new Bus(2,false,50));
+		buses.add(new Bus(3,true,48));
+		
+		int userOption = 1;
+		Scanner sc = new Scanner(System.in);
+		
+		for(Bus b:buses) {
+			b.displayBusInfo();
+		}
+		
+		
+		while(userOption == 1) {
+			System.out.println("ENTER '1' TO BOOK and '2' TO EXIT...." );
+			userOption = sc.nextInt();
+			
+			if(userOption == 2) {
+				System.out.println("<<... THANK YOU ...>>");
+			}
+		
+			if(userOption == 1) {
+				Booking booking = new Booking();
+				if(booking.isAvailable(bookings,buses)) {
+					bookings.add(booking);
+					System.out.println("YOUR BOOKING IS CONFIRMED..!!!");
+				}
+				else
+					System.out.println("SORRY ! BUS is FULL. Try for Another Bus or Date !!!");
+			}
+		}
+
+	}
+
+}
